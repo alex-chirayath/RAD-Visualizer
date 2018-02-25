ed2e25dbb2d36da09c8ac12511a7c7a790675748
dakshil <dakshil@users.noreply.github.com>
2018-02-25 09:30:10 -0600

    Revert "Added code fragements for LR and DT"
    This reverts commit 0c8c9e3e8fecbeba071fcef434c1f3d11974666f.

--- RADVisualizer/ui.R
+++ RADVisualizer/ui.R
@@ -1,16 +1,17 @@
 library(shiny)
 library(shinydashboard)
 library(colourpicker)
+library(leaflet)
 
 dashboardPage(
   dashboardHeader(),
   dashboardSidebar(
     sidebarMenu(
       radioButtons("delimFile", "Select file format:",
-                   choices = c("csv", "tsv")),
+                   choices = c("csv", "tsv"), inline = TRUE),
       fileInput(
         "file",
-        "Upload data file in selected format: ",
+        "Upload data file ",
         accept = c(
           "text/tsv",
           "text/tab-separated-values,text/plain",
@@ -21,10 +22,37 @@ dashboardPage(
         )
       ),
       menuItem("Summary", tabName = "summary"),
-      menuItem("Scatter Plot", tabName = "scatterPlot"),
-      menuItem("Bar Plot", tabName = "barPlot"),
-      menuItem("Box Plot", tabName = "boxPlot"),
-      menuItem("Histograms", tabName = "histograms")
+      menuItem("Data Visualization",
+        menuSubItem("Scatter Plot", tabName = "scatterPlot"),
+        menuSubItem("Bar Plot", tabName = "barPlot"),
+        menuSubItem("Box Plot", tabName = "boxPlot"),
+        menuSubItem("Histograms", tabName = "histograms"),
+        menuSubItem("Geospatial Plot", tabName = "geospatialmap")
+      ),
+      menuItem("Machine Learning",
+        radioButtons("testFileOpt", "Upload or split train set:", choices = c("Upload", "Split"),
+                     inline = TRUE),
+        conditionalPanel(condition = "input.testFileOpt == 'Upload'",
+                         radioButtons("delimTestFile", "Select file format:",
+                                      choices = c("csv", "tsv"), inline = TRUE),
+                         fileInput("testFile",
+                                   "Upload test file ",
+                                   accept = c(
+                                     "text/tsv",
+                                     "text/tab-separated-values,text/plain",
+                                     ".txt",
+                                     ".tsv",
+                                     "text/comma-separated-values,text/plain",
+                                     ".csv"
+                                   ))
+        ),
+        conditionalPanel(condition = "input.testFileOpt == 'Split'",
+                         numericInput("trainRatio", "Enter train size (0 - 1)",
+                                      min = 0, max = 1.0, step = 0.05, value = 0.7)
+        ),
+        menuSubItem("Linear Regression", tabName = "linearRegression"),
+        menuSubItem("Decision Tree", tabName = "decisionTree")
+      )
     )
   ),
   dashboardBody(
@@ -91,7 +119,60 @@ dashboardPage(
                 
                 box("Code", verbatimTextOutput("histPlotCode"))
               )
-      )
+      ),
+      
+      tabItem(tabName = "geospatialmap",
+              fluidRow(
+                box("Inputs for Geospatial map",
+                    uiOutput("geoVariableLatInput"),
+                    uiOutput("geoVariableLongInput"),
+                    uiOutput("geoVariableInfoInput"),
+                    actionButton("plotMap", "Plot")),
+                
+                box(leafletOutput("mapPlotOutput")),
+                
+                box("Code", verbatimTextOutput("mapPlotCode"))
+              )
+      ),
+      
+      tabItem(tabName = "linearRegression",
+              fluidRow(
+                box("LR setup",
+                  uiOutput("lrVariables"),
+                  uiOutput("lrPredictor"),
+                  actionButton("runLR", "Run")
+                ),
+                
+                box("Predictions",
+                    wellPanel(
+                      tableOutput("predictionsLR"),
+                      style = "overflow-y:scroll; max-height: 200px;
+                       overflow-x:scroll; auto"
+                    ),
+                    textOutput("mseLR"))
+              )
+      ),
+      
+      tabItem(tabName = "decisionTree",
+              fluidRow(
+                box("DT setup",
+                    uiOutput("dtVariables"),
+                    uiOutput("dtPredictor"),
+                    actionButton("runDT", "Run")
+                ),
+                
+                box("Predictions",
+                    wellPanel(
+                      tableOutput("predictionsDT"),
+                      style = "overflow-y:scroll; max-height: 200px;
+                      overflow-x:scroll; auto"
+                    )
+                ),
+                
+                box("Plot",
+                    plotOutput("plotDT"))
+                )
+        )
     )
   )
 )
